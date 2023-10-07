4135a8e80db5893034ab4051e3fb13b994198d5c
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-07 13:10:39 +0200

    spring configured and jsf used ☑️

---
+++ PlantStore/src/main/java/com/plantstore/ui/ApplicationInfo.java
@@ -0,0 +1,30 @@
+package com.plantstore.ui;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+@Named
+@ManagedBean
+@Scope("session")
+
+public class ApplicationInfo {
+	private String slogan = "Promoting Plant Diversity Through Education";
+
+	/**
+	 * @return the slogan
+	 */
+	public String getSlogan() {
+		return slogan;
+	}
+
+	/**
+	 * @param slogan the slogan to set
+	 */
+	public void setSlogan(String slogan) {
+		this.slogan = slogan;
+	}
+
+}
